14cf7bcae84864b0f0f8f47b20b64ad5a295e332
Tvang93 <vangt39@gmail.com>
2025-08-06 11:00:25 -0700

    added toast; emailjs now correctly sends name and email

--- src/app/components/EmailJSComponent.tsx
+++ src/app/components/EmailJSComponent.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 
 export const EmailJSComponent = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -11,28 +12,50 @@ export const EmailJSComponent = () => {
 
     if (!form.current) return;
     emailjs
-    .sendForm("service_ks51k5y", "template_5t2f0mb", form.current, {
-      publicKey: "TJqrvdUUaKQcBlm66",
-    })
-    .then(
-      () => {
-        console.log("SUCCESS!");
-      },
-      (error) => {
-        console.log("FAILED...", error.text);
-      }
-    );
+      .sendForm("service_ks51k5y", "template_5t2f0mb", form.current, {
+        publicKey: "TJqrvdUUaKQcBlm66",
+      })
+      .then(
+        () => {
+          toast.success("Message Successfully Sent!");
+          form.current?.reset(); 
+        },
+        (error) => {
+          toast.error("Unable To Send Message.", error.text);
+        }
+      );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="flex flex-col items-start mt-4 gap-2"
+    >
       <label htmlFor="name">Name</label>
-      <input id="name" type="text" name="user_name" />
+      <input
+        id="name"
+        type="text"
+        name="name"
+        placeholder="Your Name"
+        className="p-2 mb-2 border border-[#4B2E2E] rounded min-w-120"
+      />
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" name="user_email" />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Your Email"
+        className="p-2 mb-2 border border-[#4B2E2E] rounded min-w-120"
+      />
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" />
-      <input type="submit" value="Send" />
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Your Message"
+        className="p-2 mb-2 border border-[#4B2E2E] rounded min-w-120"
+      />
+      <input type="submit" value="Send" className="self-center bg-[#4B2E2E] text-[#D8CFC4] p-2 rounded cursor-pointer mt-3"/>
     </form>
   );
 };
